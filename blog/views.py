aadbbc5258f6ae3a405687f82e94a320130cb71f
Blackzafiro <blackzafiro.net@gmail.com>
2016-02-21 22:20:34 -0600

    Todo lo escencial (Sin comentarios xP)

--- blog/views.py
+++ blog/views.py
@@ -46,3 +46,15 @@ def post_edit(request, pk):
         else:
             form = PostForm(instance=post)
         return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog.views.post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog.views.post_list')
